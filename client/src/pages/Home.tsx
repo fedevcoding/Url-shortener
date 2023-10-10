ba22e8584e32abcf9afba2e5ff521748a47cddc5
Federico <federicocavallini100@gmail.com>
2023-10-10 21:42:46 +0200

    start ui

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <h1>Short URL</h1>;
+};
+
+export default Home;
--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,8 @@
-console.log("Hello via Bun!");
+const server = Bun.serve({
+  port: process.env.PORT || 3000,
+  fetch(req) {
+    return new Response("Bun!");
+  },
+});
+
+console.log(`Server listening on port ${server.port}`);
