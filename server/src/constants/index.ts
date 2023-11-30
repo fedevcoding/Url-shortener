3cfe9d0c45aef75cb3ac996c13512747cb371466
Federico <federicocavallini100@gmail.com>
2023-11-30 09:46:57 +0100

    add rate limit

--- server/src/constants/index.ts
+++ server/src/constants/index.ts
@@ -1,4 +1,13 @@
 import { getEnv } from "@/utils";
+import { rateLimit } from "express-rate-limit";
 
 export const CLIENT_URL = getEnv("CLIENT_URL");
 export const CLIENT_NAME = getEnv("CLIENT_NAME");
+
+export const LIMITER = rateLimit({
+  windowMs: 1 * 30 * 1000,
+  limit: 15,
+  keyGenerator: function (req) {
+    return req.clientIp || req.ip;
+  },
+});
